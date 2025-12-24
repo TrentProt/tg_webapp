8b72b9deb54171d7b42e8af127fe2c3d6fc4933f
Данила Рябинин <ryabinin.danila355@mail.ru>
2025-12-24 13:37:01 +0300

    Add data

---
+++ backend/data.py
@@ -0,0 +1,35 @@
+CONTAINERS = [
+    {
+        "id": 1,
+        "name": "20’ стандартный",
+        "price": 1000,
+        "description": "20-ть золотой стандарт описание",
+        "images": [
+            "/static/img/20_1.jpg",
+            "/static/img/20_2.jpg",
+            "/static/img/20_3.jpg",
+        ],
+    },
+    {
+        "id": 2,
+        "name": "40’ HC",
+        "price": 1100,
+        "description": "40 НС описание",
+        "images": [
+            "/static/img/40hc_1.jpg",
+            "/static/img/40hc_2.jpg",
+            "/static/img/40hc_3.jpg",
+        ],
+    },
+    {
+        "id": 3,
+        "name": "40’ рефрижератор",
+        "price": 1200,
+        "description": "40 описание",
+        "images": [
+            "/static/img/ref_1.jpg",
+            "/static/img/ref_2.jpg",
+            "/static/img/ref_3.jpg",
+        ],
+    },
+]
